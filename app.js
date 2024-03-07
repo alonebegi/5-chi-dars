26a9de02e111f6a4e7e09bc6524be21992cc0fb9
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-03-07 21:57:10 +0300

    masalalar

---
+++ app.js
@@ -0,0 +1,44 @@
+// 17masala
+// function ochkolar(odiy, qiyin) {
+//   odiy = +prompt("2 achkoli gol nechta: ");
+//   qiyin = +prompt("3 achkoli gol nechta: ");
+//   let s = odiy * 2 + qiyin * 3;
+//   console.log(s);
+//   return s;
+// }
+// ochkolar();
+
+// 18masala
+// function text(one, two) {
+//   one = prompt("text kriting:");
+//   two = "Edabit";
+//   let texts = one + "" + two;
+//   console.log(texts);
+//   return texts;
+// }
+// text();
+
+// 19masala
+// function qiymat(x, y) {
+//   x = +prompt(" 1 chi soni kriting: ");
+//   y = +prompt(" 2 chi soni kriting: ");
+//   let yigindi = x + y;
+//   if (yigindi < 100) {
+//     console.log(true);
+//   } else if (yigindi > 100) {
+//     console.log(false);
+//   }
+//   return yigindi;
+// }
+// qiymat();
+
+// 21masala
+// function oyoqlar(tovuq, qoy, sigir) {
+//   tovuq = +prompt("tovuq nechta: ");
+//   qoy = +prompt("qoy nechta: ");
+//   sigir = +prompt("sigir nechta: ");
+//   let oyoq = tovuq * 2 + qoy * 4 + sigir * 4;
+//   console.log(oyoq);
+//   return oyoq;
+// }
+// oyoqlar();
